e549fb52cd7b25a1238469b77013505df740da67
shopzek <frankness1988@gmail.com>
2026-01-24 14:27:03 +0400

    Update app.js

--- app.js
+++ app.js
@@ -1,6 +1,10 @@
 pdfjsLib.GlobalWorkerOptions.workerSrc =
   "https://cdnjs.cloudflare.com/ajax/libs/pdf.js/2.16.105/pdf.worker.min.js";
 
+function toggleDark(){
+  document.body.classList.toggle("dark");
+}
+
 
 async function convertJpgToPdf() {
   const input = document.getElementById("jpgInput");
